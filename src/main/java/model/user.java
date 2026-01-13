36b4fe932e5cd74018705860a2f3fec119ebd23c
Rashmi Wijemanna <rashmiwijemanna7@gmail.com>
2026-01-13 10:13:39 +0530

    implement feilds in user class and add annotaions using lombok

--- src/main/java/model/user.java
+++ src/main/java/model/user.java
@@ -1,4 +1,17 @@
 package model;
 
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+
 public class user {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+
 }
